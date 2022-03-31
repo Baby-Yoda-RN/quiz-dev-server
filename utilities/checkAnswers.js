ee7a104c1bbf87f3a4514eba842f4f9d92c73a68
Dulaya <dulaya.saennok@gmail.com>
2022-03-31 05:51:15 -0700

    Check Answers: create check answer utility

---
+++ utilities/checkAnswers.js
@@ -0,0 +1,31 @@
+/**
+ * Compare the user answers in database vs correct answers.
+ * Calculate score: >= 80% is passing.
+ * Send result to client.
+ *
+ * Once user submits a test, client will send user's answers.
+ *
+ */
+
+export const checkAnswers = (
+  userAnswers = [{ id: 100, userAnswer: "I don't know" }],
+  correctAnswers = [
+    { id: 100, correctAnswer: "Instruction" },
+    { id: 101, correctAnswer: "True" },
+  ]
+) => {
+  // userAnswers will be provided in an array of objects with id and correctAnswer.
+  // correctAnswers will be provided in an array of objects with id and userAnswer.
+  // userAnswers = [{id:100, userAnswer: "I don't know"}];
+  // correctAnswer = [{id:100, correctAnswer: "Instruction"}];
+
+  // Filter userAnswers and check if the userAnswer matches with some of the correctAnswer.
+  const userCorrectAnswers = userAnswers.filter((userAnswerObject) =>
+    correctAnswers.some(
+      (correctAnswerObject) =>
+      userAnswerObject.userAnswer === correctAnswerObject.correctAnswer && userAnswerObject.id === correctAnswerObject.id
+    )
+  );
+
+  return userCorrectAnswers.length / correctAnswers.length;
+};
